2c0a42bcd86124eee9016f723e8efc8cb1621ede
unknown <cogjava4571@iiht.tech>
2022-12-20 13:19:20 +0530

    completed sprint 3

--- Frontend/helloworld/src/app/Components/addbook/addbook.component.ts
+++ Frontend/helloworld/src/app/Components/addbook/addbook.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 import Book from 'src/app/Entity/book'
 import  { BookService } from 'src/app/Services/book.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addbook',
@@ -14,7 +15,9 @@ export class AddbookComponent implements OnInit {
 
   
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService,private router:Router) {
+
+  }
 
   
   
@@ -23,14 +26,19 @@ export class AddbookComponent implements OnInit {
     const observables = this.bookService.createBook(this.book);
     observables.subscribe(
       (res: any) => {
+        Swal.fire("done","Book has been added successfully","success")
         console.log(res);
       }, function (error) {
         console.log(error);
         alert("Something went wrong !, Please try again");
       }
     )
+      this.allbooks();
   }
 
+allbooks(){
+  this.router.navigate(["/Get-books"])
+}
   ngOnInit(): void {
    
     
